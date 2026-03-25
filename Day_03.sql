400ea1de44db7224f6fccd5ffaff4f1231634066
SATYA RANJAN PANDA <satyaranjan1010panda@gmail.com>
2026-03-26 02:26:43 +0530

    RENAME,TRUNCATE COMMAND

--- Day_03.sql
+++ Day_03.sql
@@ -50,3 +50,50 @@ DROP COLUMN <COLUMN_NAME>;
 ALTER TABLE STUDENT_DB
 DROP COLUMN SFEE;
 
+
+-- 3. RENAME
+=============
+ - To change a table name.
+ 
+-- SYNTAX:
+===========
+RENAME <OLD TABLE NAME> TO <NEW TABLE NAME>;
+
+EXAMPLE:
+RENAME STUDENT_DB TO STUDENT;
+
+-- 4. TRUNCATE
+=================
+-- To delete all rows from a table at a time.
+-- To delete rows but not columns.
+-- Permanent deletion.
+-- Can not delete a specific row from a table.
+-- It is not allowed "WHERE" clause condition.
+
+-- SYNTAX
+==========
+ TRUNCATE TABLE <TABLE_NAME>;
+
+-- Example
+===========
+TRUNCATE TABLE STUDENT;
+
+
+-- PRIVILEGE FOR TABLESPACE OR YOU CAN SAY STORAGE
+======================================================
+-- First you should connect with SYSTEM admin, then you have to write for access TABLESPACE.
+-- Then,you can insert data over the table.
+
+-- Enter user-name: system /Satya123
+-- Last Successful login time: Wed Mar 25 2026 03:13:26 +05:30
+
+-- Connected to:
+-- Oracle Database 19c Enterprise Edition Release 19.0.0.0.0 - Production
+-- Version 19.3.0.0.0
+
+-- SQL> GRANT UNLIMITED TABLESPACE TO SATYADB;
+
+-- Grant succeeded.
+
+
+
