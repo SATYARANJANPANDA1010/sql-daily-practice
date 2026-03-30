be4959f76dfc2e6b2270c11c8a584048f60de9d3
SATYA RANJAN PANDA <satyaranjan1010panda@gmail.com>
2026-03-30 13:04:48 +0530

    DAY_08

---
+++ DAY_08.sql
@@ -0,0 +1,8 @@
+-- How to CREATE a new table from the OLD Table:
+SYNTAX:
+=========
+CREATE TABLE <TABLE_NAME> AS SELECT * FROM <OLD TABLE NAME>;
+
+Example:
+=========
+CREATE TABLE EMPLOYEE AS SELECT * FROM EMP;
